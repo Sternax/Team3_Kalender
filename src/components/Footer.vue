5530833d3b687bb767f43434898846433104d3a7
alexanderiksson <Alexerik69@gmail.com>
2025-02-21 10:12:52 +0100

    Börjat med innehåll i footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,35 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
-    <footer></footer>
+    <footer>
+        <ul>
+            <li>
+                <RouterLink to="/">Kalender</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/">Om oss</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/">Kontakta oss</RouterLink>
+            </li>
+        </ul>
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    background-color: #071826;
+    color: white;
+    padding: 4rem 0;
+    text-align: center;
+}
+
+ul {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    list-style: none;
+}
+</style>
