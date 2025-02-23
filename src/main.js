b026183b1eef357ee62b9326b0b1de3f6a608305
Sternax <136315680+Sternax@users.noreply.github.com>
2025-02-23 22:15:47 +0100

    Importerade vcalendar

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import router from "./router";
 import "./style.css";
 import App from "./App.vue";
+import "v-calendar/style.css";
+import VCalendar from "v-calendar";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(VCalendar, {}).mount("#app");
