c381529e44d31a85861a413c478332e32f304615
Sternax <136315680+Sternax@users.noreply.github.com>
2025-02-26 13:45:48 +0100

    Implementerade nya schedule-x kalendern

--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import { createApp } from "vue";
 import router from "./router";
 import "./style.css";
 import App from "./App.vue";
-import "v-calendar/style.css";
-import VCalendar from "v-calendar";
 
-createApp(App).use(router).use(VCalendar, {}).mount("#app");
+createApp(App).use(router).mount("#app");
