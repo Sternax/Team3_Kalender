b5eb0a5570977b437f2cd5e49baed51568623d7a
alexanderiksson <Alexerik69@gmail.com>
2025-03-03 13:53:15 +0100

    Uppdatera kalender

--- src/components/KalenderKomponent.vue
+++ src/components/KalenderKomponent.vue
@@ -1,16 +1,14 @@
 <script setup>
 import { ScheduleXCalendar } from "@schedule-x/vue";
 import { createEventModalPlugin } from "@schedule-x/event-modal";
-=======
-// import { createEventsServicePlugin } from "@schedule-x/events-service";
 import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
->>>>>>> c381529 (Implementerade nya schedule-x kalendern)
+import { createCurrentTimePlugin } from "@schedule-x/current-time";
 import {
-  createCalendar,
-  createViewDay,
-  createViewMonthAgenda,
-  createViewMonthGrid,
-  createViewWeek,
+    createCalendar,
+    createViewDay,
+    createViewMonthAgenda,
+    createViewMonthGrid,
+    createViewWeek,
 } from "@schedule-x/calendar";
 import "@schedule-x/theme-default/dist/index.css";
 
@@ -18,45 +16,37 @@ import "@schedule-x/theme-default/dist/index.css";
 const date = new Date();
 let currentDate = date.toJSON().slice(0, 10);
 
-<<<<<<< HEAD
-=======
-// const eventsServicePlugin = createEventsServicePlugin();
->>>>>>> c381529 (Implementerade nya schedule-x kalendern)
 const eventModal = createEventModalPlugin();
 
 const calendarApp = createCalendar({
-  selectedDate: `${currentDate}`, // Hämtar dagens datum från funktionen ovan och sätter det som datumet som visas i kalendern
-  views: [
-    createViewDay(),
-    createViewWeek(),
-    createViewMonthGrid(),
-    createViewMonthAgenda(),
-  ],
-<<<<<<< HEAD
-  plugins: [
-    eventModal,
-    createDragAndDropPlugin(),
-    createCurrentTimePlugin({
-      fullWeekWidth: true,
-    }),
-  ],
-=======
-  plugins: [eventModal, createDragAndDropPlugin()],
->>>>>>> c381529 (Implementerade nya schedule-x kalendern)
-  events: [
-    {
-      id: 1,
-      title: "Fredagsmöte",
-      start: "2025-02-28 10:00",
-      end: "2025-02-28 11:30",
-    },
-    {
-      id: 2,
-      title: "Sprintplanering",
-      start: "2025-03-03 12:00",
-      end: "2025-03-03 13:00",
-    },
-  ],
+    selectedDate: `${currentDate}`, // Hämtar dagens datum från funktionen ovan och sätter det som datumet som visas i kalendern
+    views: [
+        createViewDay(),
+        createViewWeek(),
+        createViewMonthGrid(),
+        createViewMonthAgenda(),
+    ],
+    plugins: [
+        eventModal,
+        createDragAndDropPlugin(),
+        createCurrentTimePlugin({
+            fullWeekWidth: true,
+        }),
+    ],
+    events: [
+        {
+            id: 1,
+            title: "Fredagsmöte",
+            start: "2025-02-28 10:00",
+            end: "2025-02-28 11:30",
+        },
+        {
+            id: 2,
+            title: "Sprintplanering",
+            start: "2025-03-03 12:00",
+            end: "2025-03-03 13:00",
+        },
+    ],
 });
 
 eventModal.close();
@@ -70,21 +60,18 @@ eventModal.close();
 
 <style scoped>
 .sx-vue-calendar-wrapper {
-  width: 1200px;
-  max-width: 100vw;
-  height: 800px;
-  max-height: 90vh;
-  margin-top: 50px;
+    width: 1200px;
+    max-width: 100vw;
+    height: 800px;
+    max-height: 90vh;
+    margin-top: 50px;
 }
-<<<<<<< HEAD
 
 @media screen and (max-width: 768px) {
-  .sx-vue-calendar-wrapper {
-    margin-top: 80px;
-    width: 100%;
-    height: 600px;
-  }
+    .sx-vue-calendar-wrapper {
+        margin-top: 80px;
+        width: 100%;
+        height: 600px;
+    }
 }
-=======
->>>>>>> c381529 (Implementerade nya schedule-x kalendern)
 </style>
